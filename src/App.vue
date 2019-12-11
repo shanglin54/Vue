1fa44f0ebd38bd7a0363718cc616b9516ada8d38
shanglin54 <827362381@qq.com>
2019-12-11 23:45:28 +0800

    3次修改

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id="app">
+        <img src alt />
+        <test></test>
+    </div>
+</template>
+
+<script>
+import test from "./components/test.vue";
+
+export default {
+    name: "app",
+    components: {
+        test
+    }
+};
+</script>
+
+<style>
+#app {
+    font-family: "Avenir", Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+</style>
---
+++ src/components/test.vue
@@ -0,0 +1,57 @@
+<template>
+    <div id="watch-example">
+        <p>
+            Ask a yes/no question:
+            <input v-model="question">
+        </p>
+        <p>{{ answer }}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "test",
+        props: {},
+        data() {
+            return {
+                question: '',
+                answer: 'I cannot give you an answer until you ask a question!'
+            }
+        },
+        watch: {
+            // 如果 `question` 发生改变，这个函数就会运行
+            question: function (newQuestion, oldQuestion) {
+                this.answer = 'Waiting for you to stop typing...'
+                this.debouncedGetAnswer()
+            }
+        },
+        created: function () {
+
+            this.debouncedGetAnswer = _.debounce(this.getAnswer, 500)
+        },
+        methods: {
+            getAnswer: function () {
+                if (this.question.indexOf('?') === -1) {
+                    this.answer = 'Questions usually contain a question mark. ;-)'
+                    return
+                }
+                this.answer = 'Thinking...'
+                var vm = this
+                axios.get('https://yesno.wtf/api')
+                    .then(function (response) {
+                        vm.answer = _.capitalize(response.data.answer)
+                    })
+                    .catch(function (error) {
+                        vm.answer = 'Error! Could not reach the API. ' + error
+                    })
+            }
+        }
+    }
+
+</script>
+
+<style scoped>
+    .red {
+        color: red;
+    }
+</style>
